f02671b3d5b5a585f2292550fd9ce9fd14fc4a89
Ruslan <r.gusseinov@mail.ru>
2020-11-18 19:55:43 +0600

    Added pizza block component and types & sizes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './scss/app.scss'
 import {Route} from 'react-router-dom'
 import {Header} from './components'
 import {Home, Cart} from './pages'
 
-class App extends React.Component {
+function App(){
 
-  render(){
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+ 
+      fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then((data) => setPizzas(data.pizzas))
+      
+  }, [])
+
+  // console.log(`data=`, pizzas)
+ 
     return (
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route path="/" component={Home} exact />
+          <Route path="/" component={() => <Home items={pizzas} />} exact />
           <Route path="/cart" component={Cart} exact /> 
         </div>
       </div>
     );
-  }
+ 
   
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ export {default as Header} from './Header'
 export {default as Categories} from './Categories'
 export {default as SortPopup} from './SortPopup'
 export {default as Home} from '../pages/Home'
+export {default as PizzaBlock} from './PizzaBlock'
+
 
