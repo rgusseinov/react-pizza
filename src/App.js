55b714ca662dad8f02e69dd646c850a05435f84b
Ruslan <r.gusseinov@mail.ru>
2020-11-20 19:11:30 +0600

    Added mapStateToProps and mapDispatchToProps function

--- src/App.js
+++ src/App.js
@@ -4,25 +4,19 @@ import {Route} from 'react-router-dom'
 import {Header} from './components'
 import {Home, Cart} from './pages'
 import axios from 'axios'
+import {connect} from 'react-redux'
+// import store from './redux/store'
+import {setPizzas as setPizzasAction} from './redux/actions/pizzas'
 
-function App(){
+/* function App(){
 
   const [pizzas, setPizzas] = useState([])
-
   useEffect(() => {
- 
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
       setPizzas(data.pizzas)
     })
-    
-/*       fetch('http://localhost:3000/db.json')
-      .then((response) => response.json())
-      .then((data) => setPizzas(data.pizzas)) */
-      
   }, [])
 
-  // console.log(`data=`, pizzas)
- 
     return (
       <div className="wrapper">
         <Header />
@@ -32,9 +26,44 @@ function App(){
         </div>
       </div>
     );
- 
+
+} */
+
+class App extends React.Component {
   
+  // const [pizzas, setPizzas] = useState([])
+
+  componentDidMount(){
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      this.props.sohranitPizi(data.pizzas)
+      // console.log(`data`, data.pizzas)
+    })
+  }
+  render(){
+    // console.log(this.props.items)
+
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Route path="/" component={() => <Home items={this.props.items} />} exact />
+          <Route path="/cart" component={Cart} exact /> 
+        </div>
+      </div>
+    )
+  }
 }
 
+const mapStateToProps = (state) => {
+  // console.log(`items`, state.pizzas.items)
+  return {
+    items: state.pizzas.items
+  }
+}
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    sohranitPizi: (items) => dispatch(setPizzasAction(items)) 
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
